962e7bdedb7031512268906ad414d8840319c29b
manalaih <hila_manalai@berea.edu>
2020-04-23 23:31:15 -0400

    working on map fragment

--- app/src/main/java/com/example/kidseat/fragments/MapFragment.java
+++ app/src/main/java/com/example/kidseat/fragments/MapFragment.java
@@ -1,16 +1,20 @@
 package com.example.kidseat.fragments;
 
 
+import android.content.Context;
 import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.kidseat.R;
+import com.example.kidseat.organizer_activities.AddEventActivity;
+import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -18,6 +22,23 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.libraries.places.api.Places;
+import com.google.android.libraries.places.api.model.Place;
+import com.google.android.libraries.places.api.net.FetchPlaceRequest;
+import com.google.android.libraries.places.api.net.PlacesClient;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+
+import static android.media.CamcorderProfile.get;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -27,9 +48,17 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
     private static final String TAG = "MapFragment";
 
     private GoogleMap mMap;
-    private LatLng eventLocation;
     private float zoom;
     private CameraUpdate cameraUpdate;
+
+    ArrayList<Object> latLngs_list = new ArrayList<>();
+
+    FirebaseFirestore mFirestore;
+
+//    LatLng eventLocation = new LatLng(37.573210, -84.286957);
+//    LatLng another = new LatLng(37.570693, -84.289817);
+//    LatLng loc2 = new LatLng(37.572000, -84.287500);
+//    LatLng loc3 = new LatLng(37.573700, -84.286000);
 
 
     public MapFragment() {
@@ -38,7 +67,12 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);}
+        super.onCreate(savedInstanceState);
+
+        mFirestore = FirebaseFirestore.getInstance();
+        getLatLngFromFireStore();
+
+    }
 
 
     @Override
@@ -60,35 +94,37 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
         mapFragment.getMapAsync(this);
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
+    public void getLatLngFromFireStore(){
+        mFirestore.collection("events")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : Objects.requireNonNull(task.getResult())) {
+                                String latlng = (String) document.get("Latlng");
+                                latLngs_list.add(latlng);
+                                Log.d(TAG, "onComplete" + latlng);
+                            }
+                        } else {
+                            Log.d(TAG, "Error getting documents: ", task.getException());
+                        }
+                    }
+                });
+    }
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
         zoom = 15;
 
         // Add a marker in Sydney and move the camera 37.573210, -84.286957
-        eventLocation = new LatLng(37.573210, -84.286957);
-        LatLng another = new LatLng(37.570693, -84.289817);
-        LatLng loc2 = new LatLng(37.572000, -84.287500);
-        LatLng loc3 = new LatLng(37.573700, -84.286000);
-        mMap.addMarker(new MarkerOptions().position(eventLocation).title("Community Garden"));
-        mMap.addMarker(new MarkerOptions().position(loc2).title("Deep Green Hall"));
-        mMap.addMarker(new MarkerOptions().position(loc3).title("Woods-Penniman"));
-        mMap.addMarker(new MarkerOptions()
-                .position(another)
-                .title("Community School"));
 
-
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(eventLocation, zoom));
-
+        for (int i=0; i < latLngs_list.size(); i++){
+            mMap.addMarker(new MarkerOptions().position((LatLng) latLngs_list.get(i)).title("Marker"));
+            mMap.moveCamera(CameraUpdateFactory.zoomTo(zoom));
+            mMap.moveCamera(CameraUpdateFactory.newLatLng((LatLng) latLngs_list.get(i)));
+        }
     }
 
 }
