6969d52e7baa5bacfeb67da8872735df1ead5001
ovezovs <shageldi.ovezov@gmail.com>
2020-11-11 12:20:38 -0500

    added firebase functions

---
+++ functions/index.js
@@ -0,0 +1,29 @@
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//   functions.logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
+
+// The Cloud Functions for Firebase SDK to create Cloud Functions and setup triggers.
+const functions = require('firebase-functions');
+
+// The Firebase Admin SDK to access Cloud Firestore.
+const admin = require('firebase-admin');
+admin.initializeApp();
+
+exports.createUser = functions.firestore
+    .document('events/{eventId}')
+    .onCreate((snap, context) => {
+      // Get an object representing the document
+      // e.g. {'name': 'Marie', 'age': 66}
+      const newValue = snap.data();
+
+      // access a particular field as you would any JS property
+      const name = newValue.name;
+
+      // perform desired operations ...
+
+    });
+
