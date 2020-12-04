a118299f208171c5beb2d04512e7035c367c5f90
ovezovs <shageldi.ovezov@gmail.com>
2020-12-04 15:41:08 -0500

    firebase cloud function now properly sends notifications based on fcm tokens

--- functions/index.js
+++ functions/index.js
@@ -11,30 +11,72 @@ const functions = require('firebase-functions');
 
 // The Firebase Admin SDK to access Cloud Firestore.
 const admin = require('firebase-admin');
-admin.initializeApp(functions.config().firebase);
+admin.initializeApp();
+let db = admin.firestore();
+let allTokens = [];
 
-exports.sendNotification = functions.firestore
-    .document('events/{eventId}')
-    .onCreate(async (snap, context) => {
-      // Get an object representing the document
-      // e.g. {'name': 'Marie', 'age': 66}
-      const newDocument = snap.data();
+async function getAll() {
 
-      // access a particular field as you would any JS property
-      let name = newDocument.name;
-      let content = "Notification content";
+  const users = db.collection('users');
+  const snapshot = await users.get();
+  snapshot.forEach(doc => {
+    if (doc.data().fcmToken) {
+      console.log("Token:", doc.data().fcmToken);
+      allTokens.push(doc.data().fcmToken);
+      console.log("tokens in getAll", allTokens);
+    } else {
+      console.log("No Token!");
+    }
+  });
 
-      let message = {
-        notification: {
-          title: name,
-          body: content
-        },
-        topic: "New Event",
-      };
-
-      let response = await admin.messaging().send(message);
-      console.log(response);
+}
 
 
-    });
+exports.sendNotificationToToken = functions.firestore
+  .document('events/{eventId}')
+  .onCreate(async (snap, context) => {
+    const newDocument = snap.data();
 
+    // access a particular field as you would any JS property
+    let name = newDocument.name;
+    let content = "Notification content";
+    await getAll();
+    console.log("tokens in sendNotificationToken", allTokens);
+
+    let message = {
+      notification: {
+        title: name,
+        body: content
+      },
+      tokens: allTokens,
+    };
+
+    let response = await admin.messaging().sendMulticast(message);
+    console.log(response);
+
+  });
+
+
+// exports.sendNotification = functions.firestore
+//   .document('events/{eventId}')
+//   .onCreate(async (snap, context) => {
+//     // Get an object representing the document
+//     // e.g. {'name': 'Marie', 'age': 66}
+//     const newDocument = snap.data();
+
+//     // access a particular field as you would any JS property
+//     let name = newDocument.name;
+//     let content = "Notification content";
+
+//     let message = {
+//       notification: {
+//         title: name,
+//         body: content
+//       },
+//       topic: "New Event",
+//     };
+
+//     let response = await admin.messaging().send(message);
+//     console.log(response);
+
+//   });
